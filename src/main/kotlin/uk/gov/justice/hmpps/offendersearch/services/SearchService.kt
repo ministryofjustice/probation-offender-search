42f75fe1150c26edd132877fccf108b5a96cdf62
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2020-04-30 14:22:58 +0100

    pat-719 fix for cro and pnc searches containing /

--- src/main/kotlin/uk/gov/justice/hmpps/offendersearch/services/SearchService.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offendersearch/services/SearchService.kt
@@ -18,77 +18,77 @@ import java.util.*
 
 @Service
 class SearchService @Autowired constructor(private val hlClient: SearchClient, private val mapper: ObjectMapper) {
-    companion object {
-        val log: Logger = LoggerFactory.getLogger(this::class.java)
-        private const val MAX_SEARCH_RESULTS = 100
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+    private const val MAX_SEARCH_RESULTS = 100
+  }
+
+  fun performSearch(searchOptions: SearchDto): List<OffenderDetail> {
+    validateSearchForm(searchOptions)
+    val searchRequest = SearchRequest("offender")
+    val searchSourceBuilder = SearchSourceBuilder()
+    // Set the maximum search result size (the default would otherwise be 10)
+    searchSourceBuilder.size(MAX_SEARCH_RESULTS)
+    val matchingAllFieldsQuery = buildMatchWithAllProvidedParameters(searchOptions)
+    searchSourceBuilder.query(matchingAllFieldsQuery)
+    searchRequest.source(searchSourceBuilder)
+    val response = hlClient.search(searchRequest)
+    return getSearchResult(response)
+  }
+
+  protected fun buildMatchWithAllProvidedParameters(searchOptions: SearchDto): BoolQueryBuilder {
+    val matchingAllFieldsQuery = QueryBuilders
+            .boolQuery()
+    with(searchOptions) {
+      croNumber.takeIf { !it.isNullOrBlank() }?.let {
+        matchingAllFieldsQuery
+                .mustKeyword(it.toLowerCase(), "otherIds.croNumberLowercase")
+      }
+      pncNumber.takeIf { !it.isNullOrBlank() }?.let {
+        matchingAllFieldsQuery
+                .mustMultiMatchKeyword(it.canonicalPNCNumber(), "otherIds.pncNumberLongYear", "otherIds.pncNumberShortYear")
+      }
+      matchingAllFieldsQuery
+              .mustWhenPresent("otherIds.nomsNumber", nomsNumber)
+      matchingAllFieldsQuery
+              .mustWhenPresent("otherIds.crn", crn)
+      matchingAllFieldsQuery
+              .mustWhenPresent("firstName", firstName)
+      matchingAllFieldsQuery
+              .mustWhenPresent("surname", surname)
+      matchingAllFieldsQuery
+              .mustWhenPresent("dateOfBirth", dateOfBirth)
     }
 
-    fun performSearch(searchOptions: SearchDto): List<OffenderDetail> {
-        validateSearchForm(searchOptions)
-        val searchRequest = SearchRequest("offender")
-        val searchSourceBuilder = SearchSourceBuilder()
-        // Set the maximum search result size (the default would otherwise be 10)
-        searchSourceBuilder.size(MAX_SEARCH_RESULTS)
-        val matchingAllFieldsQuery = buildMatchWithAllProvidedParameters(searchOptions)
-        searchSourceBuilder.query(matchingAllFieldsQuery)
-        searchRequest.source(searchSourceBuilder)
-        val response = hlClient.search(searchRequest)
-        return getSearchResult(response)
-    }
+    return matchingAllFieldsQuery
+  }
 
-    protected fun buildMatchWithAllProvidedParameters(searchOptions: SearchDto): BoolQueryBuilder {
-        val matchingAllFieldsQuery = QueryBuilders
-                .boolQuery()
-        with(searchOptions) {
-            croNumber.takeIf { !it.isNullOrBlank() }?.let {
-                matchingAllFieldsQuery
-                        .mustKeyword(it.toLowerCase(), "otherIds.croNumberLowercase")
-            }
-            pncNumber.takeIf { !it.isNullOrBlank() }?.let {
-                matchingAllFieldsQuery
-                        .mustMultiMatchKeyword(it.canonicalPNCNumber(), "otherIds.pncNumberLongYear", "otherIds.pncNumberShortYear")
-            }
-            matchingAllFieldsQuery
-                    .mustWhenPresent("otherIds.nomsNumber", nomsNumber)
-            matchingAllFieldsQuery
-                    .mustWhenPresent("otherIds.crn", crn)
-            matchingAllFieldsQuery
-                    .mustWhenPresent("firstName", firstName)
-            matchingAllFieldsQuery
-                    .mustWhenPresent("surname", surname)
-            matchingAllFieldsQuery
-                    .mustWhenPresent("dateOfBirth", dateOfBirth)
-        }
-
-        return matchingAllFieldsQuery
+  private fun validateSearchForm(searchOptions: SearchDto) {
+    if (!searchOptions.isValid) {
+      log.warn("Invalid search  - no criteria provided")
+      throw BadRequestException("Invalid search  - please provide at least 1 search parameter")
     }
+  }
 
-    private fun validateSearchForm(searchOptions: SearchDto) {
-        if (!searchOptions.isValid) {
-            log.warn("Invalid search  - no criteria provided")
-            throw BadRequestException("Invalid search  - please provide at least 1 search parameter")
-        }
+  private fun getSearchResult(response: SearchResponse): List<OffenderDetail> {
+    val searchHit = response.hits.hits
+    val offenderDetailList = ArrayList<OffenderDetail>()
+    if (searchHit.isNotEmpty()) {
+      Arrays.stream(searchHit)
+              .forEach { hit: SearchHit ->
+                offenderDetailList
+                        .add(parseOffenderDetail(hit.sourceAsString))
+              }
     }
+    return offenderDetailList
+  }
 
-    private fun getSearchResult(response: SearchResponse): List<OffenderDetail> {
-        val searchHit = response.hits.hits
-        val offenderDetailList = ArrayList<OffenderDetail>()
-        if (searchHit.isNotEmpty()) {
-            Arrays.stream(searchHit)
-                    .forEach { hit: SearchHit ->
-                        offenderDetailList
-                                .add(parseOffenderDetail(hit.sourceAsString))
-                    }
-        }
-        return offenderDetailList
+  private fun parseOffenderDetail(src: String): OffenderDetail {
+    return try {
+      mapper.readValue(src, OffenderDetail::class.java)
+    } catch (t: Throwable) {
+      throw RuntimeException(t)
     }
-
-    private fun parseOffenderDetail(src: String): OffenderDetail {
-        return try {
-            mapper.readValue(src, OffenderDetail::class.java)
-        } catch (t: Throwable) {
-            throw RuntimeException(t)
-        }
-    }
+  }
 
 }
