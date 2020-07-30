568f6ed92cec0d95594628ef1bdd68a3b94398fd
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-07-30 09:27:25 +0100

    DT-839 Simplify index build in preparation to change mapping

--- src/test/kotlin/uk/gov/justice/hmpps/offendersearch/util/LocalStackHelper.kt
+++ src/test/kotlin/uk/gov/justice/hmpps/offendersearch/util/LocalStackHelper.kt
@@ -1,40 +1,43 @@
 package uk.gov.justice.hmpps.offendersearch.util
 
-import com.amazonaws.util.IOUtils
-import com.google.gson.JsonParser
-import org.apache.http.HttpHeaders
-import org.apache.http.entity.StringEntity
-import org.apache.http.message.BasicHeader
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.matches
 import org.awaitility.kotlin.untilCallTo
-import org.elasticsearch.client.ResponseException
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.action.ingest.PutPipelineRequest
+import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
+import org.elasticsearch.client.core.CountRequest
+import org.elasticsearch.client.indices.CreateIndexRequest
+import org.elasticsearch.client.indices.GetIndexRequest
+import org.elasticsearch.client.indices.PutMappingRequest
+import org.elasticsearch.common.bytes.BytesArray
+import org.elasticsearch.common.xcontent.XContentType.JSON
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.nio.file.Files
-import java.nio.file.Paths
 import java.util.*
 
-class LocalStackHelper(var esClient: RestHighLevelClient) {
+class LocalStackHelper(private val esClient: RestHighLevelClient, private val mappingVersion: String = "v1") {
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
+    const val indexName = "offender"
   }
 
   fun loadData() {
     rebuildIndex()
-    loadOffender("1", loadFile("src/test/resources/elasticsearchdata/john-smith.json"))
-    loadOffender("2", loadFile("src/test/resources/elasticsearchdata/jane-smith.json"))
-    loadOffender("3", loadFile("src/test/resources/elasticsearchdata/sam-jones-deleted.json"))
-    loadOffender("4", loadFile("src/test/resources/elasticsearchdata/antonio-gramsci-n01.json"))
-    loadOffender("5", loadFile("src/test/resources/elasticsearchdata/antonio-gramsci-n02.json"))
-    loadOffender("6", loadFile("src/test/resources/elasticsearchdata/antonio-gramsci-n03.json"))
-    loadOffender("7", loadFile("src/test/resources/elasticsearchdata/anne-gramsci-n02.json"))
-    loadOffender("8", loadFile("src/test/resources/elasticsearchdata/antonio-gramsci-c20.json"))
+    loadOffender("1", "/elasticsearchdata/john-smith.json".resourceAsString())
+    loadOffender("2", "/elasticsearchdata/jane-smith.json".resourceAsString())
+    loadOffender("3", "/elasticsearchdata/sam-jones-deleted.json".resourceAsString())
+    loadOffender("4", "/elasticsearchdata/antonio-gramsci-n01.json".resourceAsString())
+    loadOffender("5", "/elasticsearchdata/antonio-gramsci-n02.json".resourceAsString())
+    loadOffender("6", "/elasticsearchdata/antonio-gramsci-n03.json".resourceAsString())
+    loadOffender("7", "/elasticsearchdata/anne-gramsci-n02.json".resourceAsString())
+    loadOffender("8", "/elasticsearchdata/antonio-gramsci-c20.json".resourceAsString())
     waitForOffenderLoading(8)
   }
 
-  fun loadData(offenders : List<String>) {
+  fun loadData(offenders: List<String>) {
     rebuildIndex()
 
     offenders.forEach {
@@ -46,53 +49,47 @@ class LocalStackHelper(var esClient: RestHighLevelClient) {
 
   private fun rebuildIndex() {
     destroyIndex()
-    destroyPipeline()
     buildPipeline()
     buildIndex()
   }
 
   private fun waitForOffenderLoading(expectedCount: Int) {
     await untilCallTo {
-      val response = esClient.lowLevelClient.performRequest("get", "/offender/_count", BasicHeader("any", "any"))
-      JsonParser.parseString(IOUtils.toString(response.entity.content)).asJsonObject["count"].asInt
-    } matches { it == expectedCount }
+      esClient.count(CountRequest(indexName), RequestOptions.DEFAULT).count
+    } matches { it == expectedCount.toLong() }
   }
 
   private fun loadOffender(key: String, offender: String) {
     log.debug("Loading offender: {}", offender)
-    esClient.lowLevelClient.performRequest("put", "/offender/document/$key?pipeline=pnc-pipeline", HashMap(), StringEntity(offender), BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json"))
+    esClient.index(IndexRequest()
+        .setPipeline("pnc-pipeline")
+        .source(offender, JSON)
+        .id(key)
+        .type("_doc")
+        .index(indexName), RequestOptions.DEFAULT)
   }
 
   private fun destroyIndex() {
     log.debug("Dropping offender index")
-    try {
-      esClient.lowLevelClient.performRequest("delete", "/offender", BasicHeader("any", "any"))
-    } catch (e: ResponseException) {
-      log.debug("destroyIndex returned ", e)
-    }
-  }
-
-  private fun destroyPipeline() {
-    try {
-      log.debug("destroy pipeline")
-      esClient.lowLevelClient.performRequest("delete", "/_ingest/pipeline/pnc-pipeline", BasicHeader("any", "any"))
-    } catch (e: ResponseException) {
-      log.debug("destroyPipeline returned ", e)
+    if (esClient.indices().exists(GetIndexRequest(indexName), RequestOptions.DEFAULT)) {
+      esClient.indices().delete(DeleteIndexRequest(indexName), RequestOptions.DEFAULT)
     }
   }
 
   private fun buildIndex() {
+    esClient.indices().create(CreateIndexRequest(indexName), RequestOptions.DEFAULT)
+    esClient.indices()
+        .putMapping(PutMappingRequest(indexName).source("/elasticsearchdata/create-mapping-$mappingVersion.json".resourceAsString(), JSON), RequestOptions.DEFAULT)
     log.debug("Build index")
-    esClient.lowLevelClient.performRequest("put", "/offender", HashMap(), StringEntity(loadFile("src/test/resources/elasticsearchdata/create-index.json")), BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json"))
   }
 
   private fun buildPipeline() {
     log.debug("Build pipeline")
-    esClient.lowLevelClient.performRequest("put", "/_ingest/pipeline/pnc-pipeline", HashMap(), StringEntity(loadFile("src/test/resources/elasticsearchdata/create-pipeline.json")), BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json"))
-  }
-
-  private fun loadFile(file: String): String {
-    return Files.readString(Paths.get(file))
+    esClient.ingest()
+        .putPipeline(PutPipelineRequest("pnc-pipeline", "/elasticsearchdata/create-pipeline.json".resourceAsByteReference(), JSON), RequestOptions.DEFAULT)
   }
 
 }
+
+private fun String.resourceAsString() = LocalStackHelper::class.java.getResource(this).readText()
+private fun String.resourceAsByteReference() = BytesArray(LocalStackHelper::class.java.getResource(this).readBytes())
