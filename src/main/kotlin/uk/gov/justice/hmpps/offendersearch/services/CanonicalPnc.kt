b92f7946acde8e64d99fff617b964204d1718c05
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-08-18 16:31:27 +0100

    DT-963 Term utility functions

--- src/main/kotlin/uk/gov/justice/hmpps/offendersearch/services/CanonicalPnc.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offendersearch/services/CanonicalPnc.kt
@@ -1,17 +1,24 @@
 package uk.gov.justice.hmpps.offendersearch.services
 
-internal fun String.canonicalPNCNumber(): String {
-  return if (this.isPNCNumber()) {
-    val (year, serial) = this.split("/")
-    val serialNumber =  serial.substring(0, serial.length - 1).toInt()
-    val checksum = this.last()
+internal fun String.canonicalPNCNumber() =
+    if (this.isPNCNumber()) {
+      toPNCNumber()
+    } else this
 
-    "$year/$serialNumber$checksum".toLowerCase()
-  } else this
-
-}
+internal fun String.canonicalPNCNumberOrNull(): String? =
+    if (this.isPNCNumber()) {
+      toPNCNumber()
+    } else null
 
 
 private fun String.isPNCNumber(): Boolean {
   return this.matches("^([0-9]{2}|[0-9]{4})/[0-9]+[a-zA-Z]".toRegex())
+}
+
+private fun String.toPNCNumber(): String {
+  val (year, serial) = this.split("/")
+  val serialNumber = serial.substring(0, serial.length - 1).toInt()
+  val checksum = this.last()
+
+  return "$year/$serialNumber$checksum".toLowerCase()
 }
