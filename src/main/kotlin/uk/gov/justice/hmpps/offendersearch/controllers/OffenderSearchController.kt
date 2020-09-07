faf9abe4d86d4a73da037344fb96d68f87944e62
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2020-09-07 14:52:19 +0100

    pat-1258 Add endpoint to allow matching on a list of Offender CRNs

--- src/main/kotlin/uk/gov/justice/hmpps/offendersearch/controllers/OffenderSearchController.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offendersearch/controllers/OffenderSearchController.kt
@@ -1,6 +1,13 @@
 package uk.gov.justice.hmpps.offendersearch.controllers
 
-import io.swagger.annotations.*
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiImplicitParam
+import io.swagger.annotations.ApiImplicitParams
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
+import io.swagger.annotations.Authorization
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Pageable
