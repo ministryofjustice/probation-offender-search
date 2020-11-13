abe850af06ec12c69d9919f1f26d1270ae6cf5db
Ryan Forsyth <ryan.forsyth@digital.justice.gov.uk>
2020-11-13 14:56:12 +0000

    upgrade gradle version to 1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.1.1"
   kotlin("plugin.spring") version "1.4.10"
 }
 
