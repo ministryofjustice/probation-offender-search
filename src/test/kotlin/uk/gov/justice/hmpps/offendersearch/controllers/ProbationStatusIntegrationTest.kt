42588987fcf440dae52c30f79edb97733e7f5cf3
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2021-02-25 13:54:02 +0000

    📃 PIC-1121 PR comments  - handle possible nulls

--- src/test/kotlin/uk/gov/justice/hmpps/offendersearch/controllers/ProbationStatusIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/hmpps/offendersearch/controllers/ProbationStatusIntegrationTest.kt
@@ -38,6 +38,26 @@ class ProbationStatusIntegrationTest : LocalstackIntegrationBase() {
     }
 
     @Test
+    fun `ProbationStatus with null fields is handled in the response`() {
+      val response = RestAssured.given()
+        .auth()
+        .oauth2(jwtAuthenticationHelper.createJwt("ROLE_COMMUNITY"))
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body("""{"crn": "X00004"}""")
+        .post("/search")
+        .then()
+        .statusCode(200)
+        .extract()
+        .body().asString()
+
+      assertThatJson(response).node("[0].otherIds.crn").isEqualTo("X00004")
+      assertThatJson(response).node("[0].probationStatus.status").isEqualTo("PREVIOUSLY_KNOWN")
+      assertThatJson(response).node("[0].probationStatus.inBreach").isAbsent()
+      assertThatJson(response).node("[0].probationStatus.preSentenceActivity").isEqualTo(false)
+      assertThatJson(response).node("[0].probationStatus.previouslyKnownTerminationDate").isAbsent()
+    }
+
+    @Test
     fun `Missing ProbationStatus is not included in the response`() {
       val response = RestAssured.given()
         .auth()
@@ -75,6 +95,26 @@ class ProbationStatusIntegrationTest : LocalstackIntegrationBase() {
       assertThatJson(response).node("matches[0].offender.probationStatus.inBreach").isEqualTo(true)
       assertThatJson(response).node("matches[0].offender.probationStatus.preSentenceActivity").isEqualTo(false)
       assertThatJson(response).node("matches[0].offender.probationStatus.previouslyKnownTerminationDate").isEqualTo("2021-02-08")
+    }
+
+    @Test
+    fun `ProbationStatus with null fields is returned from the match`() {
+      val response = RestAssured.given()
+        .auth()
+        .oauth2(jwtAuthenticationHelper.createJwt("ROLE_COMMUNITY"))
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body("""{"surname": "Gramsci", "dateOfBirth": "1988-01-06"}""")
+        .post("/match")
+        .then()
+        .statusCode(200)
+        .extract()
+        .body().asString()
+
+      assertThatJson(response).node("matches[0].offender.otherIds.crn").isEqualTo("X00004")
+      assertThatJson(response).node("matches[0].offender.probationStatus.status").isEqualTo("PREVIOUSLY_KNOWN")
+      assertThatJson(response).node("matches[0].offender.probationStatus.inBreach").isAbsent()
+      assertThatJson(response).node("matches[0].offender.probationStatus.preSentenceActivity").isEqualTo(false)
+      assertThatJson(response).node("matches[0].offender.probationStatus.previouslyKnownTerminationDate").isAbsent()
     }
 
     @Test
