559f2b1d76ce8880be294b1c14de3cf88a88e73b
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-10-05 10:39:32 +0100

    fix for swagger for address search

--- src/main/kotlin/uk/gov/justice/hmpps/offendersearch/config/DocketConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offendersearch/config/DocketConfiguration.kt
@@ -18,7 +18,6 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider
 import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider
-import uk.gov.justice.hmpps.offendersearch.controllers.OffenderSearchController
 import java.lang.reflect.Field
 import java.time.LocalDateTime
 import java.time.ZonedDateTime
