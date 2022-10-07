1261365cbfb47018cf5b49b4c6e37d578947cd5f
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2022-10-07 10:34:35 +0100

    added example to swaggerdoc (#353)

--- src/main/kotlin/uk/gov/justice/hmpps/offendersearch/addresssearch/AddressSearchController.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offendersearch/addresssearch/AddressSearchController.kt
@@ -4,6 +4,8 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
 import io.swagger.annotations.Authorization
+import io.swagger.annotations.Example
+import io.swagger.annotations.ExampleProperty
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.security.access.prepost.PreAuthorize
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.hmpps.offendersearch.BadRequestException
+import uk.gov.justice.hmpps.offendersearch.dto.OffenderMatches
 
 @Api(
   tags = ["address-search"],
@@ -36,6 +39,49 @@ class AddressSearchController(
         code = 400,
         message = "Invalid Request",
         response = BadRequestException::class
+      ),
+      ApiResponse(
+        code = 200,
+        message = "OK",
+        response = AddressSearchResponses::class,
+        examples = Example(ExampleProperty("""
+          {
+          "personAddresses": [
+              {
+                  "person": {
+                      "id": 123,
+                      "crn": "X123123",
+                      "dob": "1990-08-16",
+                      "gender": "Male"
+                  },
+                  "address": {
+                      "id": 123,
+                      "buildingName": "Burnham House",
+                      "addressNumber": "1",
+                      "streetName": "Church Road",
+                      "district": "Clarendon Park",
+                      "town": "Leicester",
+                      "county": "Leicestershire",
+                      "postcode": "LM2 1BF",
+                      "startDate": "2020-08-03",
+                      "notes": "notes text",
+                      "createdDateTime": "2020-08-11T11:00:01+01:00",
+                      "lastUpdatedDateTime": "2022-06-09T11:47:51+01:00",
+                      "status": {
+                          "code": "M",
+                          "description": "Main"
+                      },
+                      "type": {
+                          "code": "A02",
+                          "description": "Approved Premises"
+                      },
+                      "noFixedAbode": false
+                  },
+                  "matchScore": 100
+              }
+            ]
+        }
+        """, mediaType = "application/json"))
       )
     ]
   )
