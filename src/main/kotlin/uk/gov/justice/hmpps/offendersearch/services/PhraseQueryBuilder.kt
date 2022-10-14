61ba7bebdc981c51eefbc26fb2efc97d5caebee7
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2022-10-14 15:47:51 +0100

    + (#360)

--- src/main/kotlin/uk/gov/justice/hmpps/offendersearch/services/PhraseQueryBuilder.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offendersearch/services/PhraseQueryBuilder.kt
@@ -72,8 +72,9 @@ private fun phoneQueries(phrase: String): List<QueryBuilder> =
   extractPhoneNumberLikeTerms(phrase)
     .map { phoneNumberQuery(it) }
 private fun phoneNumberQuery(term: String): QueryBuilder =
-  QueryBuilders.matchQuery("contactDetails.phoneNumbers", term)
-    .boost(10f).lenient(true)
+  QueryBuilders.matchQuery("contactNumbers", term)
+    .boost(10f)
+    .lenient(true)
     .analyzer("whitespace")
 
 private fun pncNumberQuery(term: String): QueryBuilder =
