aed53abb9a7e7c3298b0804662b94b9e3438c4c4
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-10-18 09:02:50 +0100

    PI-486

--- src/main/kotlin/uk/gov/justice/hmpps/offendersearch/cvlsearch/LicenceCaseloadRequest.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offendersearch/cvlsearch/LicenceCaseloadRequest.kt
@@ -12,8 +12,9 @@ private const val MANAGER_FORENAMES_INPUT = "manager.name.forename"
 private const val MANAGER_SURNAME_INPUT = "manager.name.surname"
 private const val FIELD_REGEX =
   "^identifiers\\.crn\$|^name\\.forename\$|^name\\.surname\$|^manager\\.name\\.forename\$|^manager\\.name\\.surname\$"
-private const val FIELD_MESSAGE =
+const val FIELD_MESSAGE =
   "should be one of '$CRN_INPUT', '$FORENAME_INPUT', '$SURNAME_INPUT', '$MANAGER_FORENAMES_INPUT' or '$MANAGER_SURNAME_INPUT'"
+const val DIRECTION_MESSAGE = "should be one of 'asc' or 'desc'"
 
 data class LicenceCaseloadRequest(
   @field:NotEmpty val teamCodes: List<String>,
@@ -30,7 +31,7 @@ data class SortBy(
     message = FIELD_MESSAGE
   )
   val field: String,
-  @field:Pattern(regexp = "^asc\$|^desc\$", message = "should be one of 'asc' or 'desc'") val direction: String = "asc"
+  @field:Pattern(regexp = "^asc\$|^desc\$", message = DIRECTION_MESSAGE) val direction: String = "asc"
 )
 
 enum class SortField(val input: String, val searchField: String) {
