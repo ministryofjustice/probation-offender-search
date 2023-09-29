424dca14bd5a43b085d5241ec8131f4687d33ffc
izzyL-DJ <isaiah.lakanu@digital.justice.gov.uk>
2023-09-29 15:04:10 +0100

    ktlintformat

--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/config/OpenAPIConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/config/OpenAPIConfiguration.kt
@@ -3,16 +3,11 @@ package uk.gov.justice.hmpps.probationsearch.config
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
-import io.swagger.v3.oas.annotations.info.License
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.annotations.servers.Server
-import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.info.BuildProperties
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 
@@ -23,12 +18,12 @@ import org.springframework.http.HttpHeaders
     contact = io.swagger.v3.oas.annotations.info.Contact(
       name = "Probation Integration Team",
       email = "probation-integration-team@digital.justice.gov.uk",
-      url = "https://mojdt.slack.com/archives/C02HQ4M2YQN" // #probation-integration-tech Slack channel
+      url = "https://mojdt.slack.com/archives/C02HQ4M2YQN", // #probation-integration-tech Slack channel
     ),
-    version = "1.0"
+    version = "1.0",
   ),
   servers = [Server(url = "/")],
-  security = [SecurityRequirement(name = "hmpps-auth-token")]
+  security = [SecurityRequirement(name = "hmpps-auth-token")],
 )
 @SecurityScheme(
   name = "hmpps-auth-token",
@@ -36,6 +31,6 @@ import org.springframework.http.HttpHeaders
   bearerFormat = "JWT",
   type = SecuritySchemeType.HTTP,
   `in` = SecuritySchemeIn.HEADER,
-  paramName = HttpHeaders.AUTHORIZATION
+  paramName = HttpHeaders.AUTHORIZATION,
 )
 class OpenAPIConfiguration
