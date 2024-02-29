001ca18e79cb8f19a11523d8cac4310d55505bab
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-02-29 17:32:37 +0000

    PI-1975

--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/OffenderSearchApplication.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/OffenderSearchApplication.kt
@@ -3,9 +3,11 @@ package uk.gov.justice.hmpps.probationsearch
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication(exclude = [ElasticsearchDataAutoConfiguration::class], scanBasePackages = ["uk.gov.justice.digital.hmpps"])
+@SpringBootApplication(
+  exclude = [ElasticsearchDataAutoConfiguration::class],
+  scanBasePackages = ["uk.gov.justice.digital.hmpps", "uk.gov.justice.hmpps"],
+)
 class OffenderSearchApplication
 
 fun main(args: Array<String>) {
