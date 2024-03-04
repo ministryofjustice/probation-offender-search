19c26cc560cbc1e8bc0c7808bf74a3ad03ce3892
Marcus Aspin <maspin@unilink.com>
2024-03-04 17:03:18 +0000

    PI-1987 Add examples for ids in API docs

--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/dto/IDs.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/dto/IDs.kt
@@ -1,10 +1,17 @@
 package uk.gov.justice.hmpps.probationsearch.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class IDs(
+  @Schema(example = "A123456", description = "Probation Case Reference Number")
   val crn: String,
+  @Schema(example = "2012/0052494Q", description = "Police National Computer ID")
   val pncNumber: String? = null,
+  @Schema(example = "123456/24A", description = "Criminal Records Office Number")
   val croNumber: String? = null,
+  @Schema(example = "AA123456A", description = "National Insurance Number")
   val niNumber: String? = null,
+  @Schema(example = "AA1234A", description = "Prison Offender Number")
   val nomsNumber: String? = null,
   val immigrationNumber: String? = null,
   val mostRecentPrisonerNumber: String? = null,
