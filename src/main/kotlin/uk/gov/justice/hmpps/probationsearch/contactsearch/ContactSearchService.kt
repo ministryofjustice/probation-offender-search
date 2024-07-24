4fe8e0ad4ed4579b1d6d392a3b847132ef6c033e
Marcus Aspin <maspin@unilink.com>
2024-07-24 11:51:49 +0000

    PI-2390 Initial attempt at migrating to OpenSearch java client

--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/contactsearch/ContactSearchService.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/contactsearch/ContactSearchService.kt
@@ -5,23 +5,18 @@ import io.opentelemetry.api.trace.Span
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.opensearch.data.client.orhlc.NativeSearchQuery
-import org.opensearch.data.client.orhlc.NativeSearchQueryBuilder
-import org.opensearch.data.client.orhlc.OpenSearchRestTemplate
-import org.opensearch.index.query.BoolQueryBuilder
-import org.opensearch.index.query.Operator
-import org.opensearch.index.query.QueryBuilders
-import org.opensearch.index.query.QueryBuilders.boolQuery
-import org.opensearch.index.query.SimpleQueryStringFlag
-import org.opensearch.search.fetch.subphase.highlight.HighlightBuilder
-import org.opensearch.search.sort.FieldSortBuilder
-import org.opensearch.search.sort.SortBuilders
-import org.opensearch.search.sort.SortOrder
+import org.opensearch.client.opensearch.OpenSearchClient
+import org.opensearch.client.opensearch._types.FieldValue
+import org.opensearch.client.opensearch._types.SortOptions
+import org.opensearch.client.opensearch._types.SortOrder
+import org.opensearch.client.opensearch._types.query_dsl.Operator
+import org.opensearch.client.opensearch.core.SearchRequest
+import org.opensearch.client.opensearch.core.search.HighlightField
+import org.opensearch.client.opensearch.core.search.HighlighterEncoder
+import org.opensearch.client.opensearch.core.search.TrackHits
 import org.springframework.data.domain.PageImpl
-import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
-import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 import uk.gov.justice.hmpps.probationsearch.contactsearch.ContactSearchService.SortType
@@ -31,16 +26,66 @@ import uk.gov.justice.hmpps.probationsearch.services.DeliusService
 import uk.gov.justice.hmpps.sqs.audit.HmppsAuditService
 import java.time.Instant
 
+
 @Service
 class ContactSearchService(
-  private val restTemplate: OpenSearchRestTemplate,
   private val auditService: HmppsAuditService?,
   private val objectMapper: ObjectMapper,
   private val deliusService: DeliusService,
+  private val openSearchClient: OpenSearchClient,
 ) {
 
   private val scope = CoroutineScope(Dispatchers.IO)
+
   fun performSearch(request: ContactSearchRequest, pageable: Pageable): ContactSearchResponse {
+    audit(request, pageable)
+
+    val searchRequest = SearchRequest.of { searchRequest ->
+      searchRequest
+        .query { query ->
+          query.bool { bool ->
+            bool.filter { filter -> filter.match { it.field("crn").query(FieldValue.of(request.crn)) } }
+            if (request.query.isNotEmpty()) bool.must { must ->
+              must.simpleQueryString { simpleQueryString ->
+                simpleQueryString.query(request.query)
+                  .analyzeWildcard(true)
+                  .defaultOperator(if (request.matchAllTerms) Operator.And else Operator.Or)
+                  .fields("notes", "type", "outcome", "description")
+                  .flags { it.multiple("AND|OR|PREFIX|PHRASE|PRECEDENCE|ESCAPE|FUZZY|SLOP") }
+              }
+            }
+            bool
+          }
+        }
+        .trackTotalHits(TrackHits.Builder().enabled(true).build())
+        .size(pageable.pageSize)
+        .from(pageable.offset.toInt())
+        .highlight { highlight ->
+          highlight
+            .encoder(HighlighterEncoder.Html)
+            .fields("notes", HighlightField.of { it.field("notes") })
+            .fields("type", HighlightField.of { it.field("type") })
+            .fields("outcome", HighlightField.of { it.field("outcome") })
+            .fields("description", HighlightField.of { it.field("description") })
+            .fragmentSize(200)
+        }
+        .sorted(pageable.sort.fieldSorts())
+    }
+    val searchResponse = openSearchClient.search(searchRequest, ContactSearchResult::class.java)
+    val results = searchResponse.hits().hits().mapNotNull { it.source()?.copy(highlights = it.highlight()) }
+
+    val response = PageImpl(results, pageable, searchResponse.hits().total().value())
+
+    return ContactSearchResponse(
+      response.numberOfElements,
+      response.pageable.pageNumber,
+      response.totalElements,
+      response.totalPages,
+      results,
+    )
+  }
+
+  private fun audit(request: ContactSearchRequest, pageable: Pageable) {
     val name = SecurityContextHolder.getContext().authentication.name
     auditService?.run {
       scope.launch {
@@ -66,40 +111,12 @@ class ContactSearchService(
           ContactSearchAuditRequest.PageRequest(
             pageable.pageNumber,
             pageable.pageSize,
-            fieldSorts.mapNotNull { SortType.from(it.fieldName)?.aliases?.first() }.joinToString(),
-            fieldSorts.joinToString { it.order().toString() },
+            fieldSorts.mapNotNull { SortType.from(it.field().field())?.aliases?.first() }.joinToString(),
+            fieldSorts.joinToString { it.field().order().toString() },
           ),
         ),
       )
     }
-
-    val query: NativeSearchQuery = NativeSearchQueryBuilder()
-      .withQuery(boolQuery().fromRequest(request))
-      .withPageable(PageRequest.of(pageable.pageNumber, pageable.pageSize))
-      .withTrackTotalHits(true)
-      .withHighlightBuilder(
-        HighlightBuilder()
-          .encoder("html")
-          .field("notes")
-          .field("type")
-          .field("outcome")
-          .field("description")
-          .fragmentSize(200),
-      ).sorted(fieldSorts)
-
-    val searchResponse =
-      restTemplate.search(query, ContactSearchResult::class.java, IndexCoordinates.of("contact-search-primary"))
-    val results = searchResponse.searchHits.mapNotNull { it.content.copy(highlights = it.highlightFields) }
-
-    val response = PageImpl(results, pageable, searchResponse.totalHits)
-
-    return ContactSearchResponse(
-      response.numberOfElements,
-      response.pageable.pageNumber,
-      response.totalElements,
-      response.totalPages,
-      results,
-    )
   }
 
   enum class SortType(val aliases: List<String>, val searchField: String) {
@@ -114,67 +131,32 @@ class ContactSearchService(
   }
 }
 
-private fun BoolQueryBuilder.fromRequest(request: ContactSearchRequest): BoolQueryBuilder {
-  filter(QueryBuilders.matchQuery("crn", request.crn))
-  if (request.query.isNotEmpty()) {
-    must(
-      QueryBuilders
-        .simpleQueryStringQuery(request.query)
-        .analyzeWildcard(true)
-        .defaultOperator(if (request.matchAllTerms) Operator.AND else Operator.OR)
-        .field("notes")
-        .field("type")
-        .field("outcome")
-        .field("description")
-        .flags(
-          SimpleQueryStringFlag.AND,
-          SimpleQueryStringFlag.OR,
-          SimpleQueryStringFlag.PREFIX,
-          SimpleQueryStringFlag.PHRASE,
-          SimpleQueryStringFlag.PRECEDENCE,
-          SimpleQueryStringFlag.ESCAPE,
-          SimpleQueryStringFlag.FUZZY,
-          SimpleQueryStringFlag.SLOP,
-        ),
-    )
-  }
-  return this
-}
-
 private fun Sort.Direction.toSortOrder() = when (this) {
-  Sort.Direction.ASC -> SortOrder.ASC
-  Sort.Direction.DESC -> SortOrder.DESC
+  Sort.Direction.ASC -> SortOrder.Asc
+  Sort.Direction.DESC -> SortOrder.Desc
 }
 
 private fun Sort.fieldSorts() = SortType.entries.flatMap { type ->
-  type.aliases.mapNotNull { alias ->
-    getOrderFor(alias)?.let {
-      SortBuilders.fieldSort(type.searchField).order(it.direction.toSortOrder())
-    }
-  }
+  type.aliases.mapNotNull { alias -> getOrderFor(alias)?.let { type.withOrder(it.direction.toSortOrder()) } }
 }
 
-private fun NativeSearchQueryBuilder.sorted(sorts: List<FieldSortBuilder>): NativeSearchQuery {
-  when (sorts.size) {
-    0 -> {
-      withSorts(
-        SortBuilders.fieldSort(SCORE.searchField).order(SortOrder.DESC),
-        SortBuilders.fieldSort(LAST_UPDATED_DATETIME.searchField).order(SortOrder.DESC),
-      )
-    }
+private fun SortType.withOrder(order: SortOrder?) =
+  SortOptions.of { options -> options.field { it.field(searchField).order(order) } }
 
+private fun SearchRequest.Builder.sorted(sorts: List<SortOptions>): SearchRequest.Builder {
+  when (sorts.size) {
+    0 -> sort(SCORE.withOrder(SortOrder.Desc), LAST_UPDATED_DATETIME.withOrder(SortOrder.Desc),)
     1 -> {
-      val sorted = sorts.first()
-      withSorts(
+      val sorted = sorts.single()
+      sort(
         sorted,
-        when (sorted.fieldName) {
-          in SCORE.aliases -> SortBuilders.fieldSort(LAST_UPDATED_DATETIME.searchField).order(sorted.order())
-          else -> SortBuilders.fieldSort(SCORE.searchField).order(sorted.order())
+        when (sorted.field().field()) {
+          in SCORE.aliases -> LAST_UPDATED_DATETIME.withOrder(sorted.field().order())
+          else -> SCORE.withOrder(sorted.field().order())
         },
       )
     }
-
-    else -> withSorts(sorts)
+    else -> sort(sorts)
   }
-  return build()
+  return this
 }
