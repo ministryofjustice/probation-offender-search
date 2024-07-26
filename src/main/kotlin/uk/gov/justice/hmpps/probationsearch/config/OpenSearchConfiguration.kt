eb6d51290a8d91a41d5bab790d923a9f22863013
Marcus Aspin <maspin@unilink.com>
2024-07-26 09:56:14 +0000

    PI-2359 Add support for hybrid semantic query

--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/config/OpenSearchConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/config/OpenSearchConfiguration.kt
@@ -1,13 +1,16 @@
 package uk.gov.justice.hmpps.probationsearch.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.http.HttpHost
 import org.opensearch.client.RestClient
 import org.opensearch.client.RestHighLevelClient
+import org.opensearch.client.json.jackson.JacksonJsonpMapper
 import org.opensearch.data.client.orhlc.OpenSearchRestTemplate
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
 class OpenSearchConfiguration {
@@ -20,4 +23,8 @@ class OpenSearchConfiguration {
   @Bean(name = ["elasticsearchTemplate", "elasticsearchOperations"])
   @ConditionalOnMissingBean(OpenSearchRestTemplate::class)
   fun openSearchRestTemplate(client: RestHighLevelClient) = OpenSearchRestTemplate(client)
+
+  @Bean
+  @Primary
+  fun jsonpMapper(objectMapper: ObjectMapper) = JacksonJsonpMapper(objectMapper)
 }
--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/services/DeliusService.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/services/DeliusService.kt
@@ -1,11 +1,16 @@
 package uk.gov.justice.hmpps.probationsearch.services
 
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.awaitExchangeOrNull
+import reactor.core.publisher.Mono
 import reactor.util.retry.Retry
+import uk.gov.justice.hmpps.probationsearch.NotFoundException
+import uk.gov.justice.hmpps.probationsearch.contactsearch.ContactJson
 import uk.gov.justice.hmpps.probationsearch.contactsearch.ContactSearchAuditRequest
 import java.time.Duration
 
@@ -13,11 +18,21 @@ import java.time.Duration
 class DeliusService(@Qualifier("searchAndDeliusApiWebClient") private val webClient: WebClient) {
   suspend fun auditContactSearch(contactSearchAuditRequest: ContactSearchAuditRequest) {
     webClient.post()
-      .uri("/audit/contact-search")
+      .uri("/probation-search/audit/contact-search")
       .contentType(MediaType.APPLICATION_JSON)
       .bodyValue(contactSearchAuditRequest)
       .awaitExchangeOrNull {
         it.toBodilessEntity().retryWhen(Retry.backoff(3, Duration.ofMillis(200))).onErrorComplete().subscribe()
       }
   }
+
+  fun getContacts(crn: String): List<ContactJson> = webClient.get()
+    .uri("/case/$crn/contacts")
+    .retrieve()
+    .toEntityList(ContactJson::class.java)
+    .onErrorResume(WebClientResponseException::class.java) {
+      if (it.statusCode == NOT_FOUND) Mono.empty() else Mono.error(it)
+    }
+    .retryWhen(Retry.backoff(3, Duration.ofMillis(200)))
+    .block()?.body ?: throw NotFoundException("Person with CRN of $crn not found")
 }
