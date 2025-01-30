29b39cded0e3aaccd6e06507cf4f607c875e2065
Marcus Aspin <maspin@unilink.com>
2025-01-30 13:10:55 +0000

    Add explicit 30 second timeout for virtual threads

--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/contactsearch/ContactSearchController.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/contactsearch/ContactSearchController.kt
@@ -8,10 +8,17 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import java.time.Instant
 import java.time.temporal.ChronoUnit
+import java.util.concurrent.CompletableFuture.runAsync
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/search/contacts")
-class ContactSearchController(val contactSearchService: ContactSearchService, val telemetryClient: TelemetryClient) {
+class ContactSearchController(
+  val contactSearchService: ContactSearchService,
+  val telemetryClient: TelemetryClient,
+  val virtualThreadExecutor: ExecutorService,
+) {
   @PreAuthorize("hasAnyRole('ROLE_PROBATION_CONTACT_SEARCH', 'ROLE_PROBATION_INTEGRATION_ADMIN')")
   @RequestMapping(method = [RequestMethod.GET, RequestMethod.POST])
   fun searchContact(
@@ -21,21 +28,28 @@ class ContactSearchController(val contactSearchService: ContactSearchService, va
   ): ContactSearchResponse = if (semantic) {
     contactSearchService.semanticSearch(request, pageable)
   } else {
-    Thread.ofVirtual().start {
-      val started = Instant.now()
-      val response = contactSearchService.semanticSearch(request, pageable)
-      telemetryClient.trackEvent(
-        "SemanticSearchCompleted",
-        mapOf(
-          "crn" to request.crn,
-          "query" to request.query.length.toString(),
-          "resultCount" to response.totalResults.toString(),
-        ),
-        mapOf(
-          "duration" to started.until(Instant.now(), ChronoUnit.MILLIS).toDouble()
-        ),
-      )
-    }
+    runAsync({ semanticSearch(request, pageable) }, virtualThreadExecutor)
+      .orTimeout(30, TimeUnit.SECONDS)
+      .exceptionally {
+        telemetryClient.trackException(RuntimeException(it))
+        null
+      }
     contactSearchService.keywordSearch(request, pageable)
+  }
+
+  private fun semanticSearch(request: ContactSearchRequest, pageable: Pageable) {
+    val started = Instant.now()
+    val response = contactSearchService.semanticSearch(request, pageable)
+    telemetryClient.trackEvent(
+      "SemanticSearchCompleted",
+      mapOf(
+        "crn" to request.crn,
+        "query" to request.query.length.toString(),
+        "resultCount" to response.totalResults.toString(),
+      ),
+      mapOf(
+        "duration" to started.until(Instant.now(), ChronoUnit.MILLIS).toDouble(),
+      ),
+    )
   }
 }
