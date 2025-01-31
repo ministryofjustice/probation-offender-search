460d74b30625ba76385c8648d6ba8f4e1fbd5a9e
Marcus Aspin <maspin@unilink.com>
2025-01-31 13:18:45 +0000

    PI-2526 Add Telemetry event for semantic search

--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/contactsearch/ContactSearchController.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/contactsearch/ContactSearchController.kt
@@ -1,14 +1,20 @@
 package uk.gov.justice.hmpps.probationsearch.contactsearch
 
+import com.microsoft.applicationinsights.TelemetryClient
 import org.springdoc.core.annotations.ParameterObject
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 @RestController
 @RequestMapping("/search/contacts")
-class ContactSearchController(val contactSearchService: ContactSearchService) {
+class ContactSearchController(
+  val contactSearchService: ContactSearchService,
+  val telemetryClient: TelemetryClient
+) {
   @PreAuthorize("hasAnyRole('ROLE_PROBATION_CONTACT_SEARCH', 'ROLE_PROBATION_INTEGRATION_ADMIN')")
   @RequestMapping(method = [RequestMethod.GET, RequestMethod.POST])
   fun searchContact(
@@ -16,7 +22,19 @@ class ContactSearchController(val contactSearchService: ContactSearchService) {
     @ParameterObject @PageableDefault pageable: Pageable,
     @RequestParam(defaultValue = "false") semantic: Boolean = false,
   ) = if (semantic) {
-    contactSearchService.semanticSearch(request, pageable)
+    val started = Instant.now()
+    val response = contactSearchService.semanticSearch(request, pageable)
+    telemetryClient.trackEvent(
+      "SemanticSearchCompleted",
+      mapOf(
+        "crn" to request.crn,
+        "query" to request.query.length.toString(),
+        "resultCount" to response.totalResults.toString(),
+      ),
+      mapOf(
+        "duration" to started.until(Instant.now(), ChronoUnit.MILLIS).toDouble(),
+      ),
+    )
   } else {
     contactSearchService.keywordSearch(request, pageable)
   }
