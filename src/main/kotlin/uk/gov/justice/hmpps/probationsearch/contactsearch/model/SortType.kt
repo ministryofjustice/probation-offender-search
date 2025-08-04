539192db27f2ec2b3e584e214f9215a2e3d886b9
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-08-04 09:01:22 +0100

    PI-3332: update enum to add startDateTime

--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/contactsearch/model/SortType.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/contactsearch/model/SortType.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.hmpps.probationsearch.contactsearch.model
 enum class SortType(val aliases: List<String>, val searchField: String) {
   DATE(listOf("date", "CONTACT_DATE"), "date.date"),
   LAST_UPDATED_DATETIME(listOf("lastUpdated"), "lastUpdatedDateTime"),
+  START_DATE_TIME(listOf("startDateTime"), "startDateTime"),
   SCORE(listOf("relevance", "RELEVANCE"), "_score"),
   ;
 
