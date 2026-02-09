5f979d1896d92c721c72a39ba1b627881cc2ec80
Marcus Aspin <maspin@unilink.com>
2026-02-09 15:03:43 +0000

    PI-3825 Exclude Elasticsearch configuration (#955)

--- src/main/kotlin/uk/gov/justice/hmpps/probationsearch/OffenderSearchApplication.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/probationsearch/OffenderSearchApplication.kt
@@ -1,10 +1,12 @@
 package uk.gov.justice.hmpps.probationsearch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.data.elasticsearch.autoconfigure.DataElasticsearchAutoConfiguration
+import org.springframework.boot.elasticsearch.autoconfigure.health.ElasticsearchRestHealthContributorAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.data.web.config.EnableSpringDataWebSupport
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataElasticsearchAutoConfiguration::class, ElasticsearchRestHealthContributorAutoConfiguration::class])
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 class OffenderSearchApplication
 
